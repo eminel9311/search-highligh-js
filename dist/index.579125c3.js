01299a2fadc4075b1a35ad87afb7c8111f825e09
Quanbh <buihongquan@vnpt.vn>
2024-04-09 14:26:36 +0700

    first commit

---
+++ dist/index.579125c3.js
@@ -0,0 +1,32 @@
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    function showFile() {
+        var preview = document.getElementById("show-text");
+        var file = document.querySelector("input[type=file]").files[0];
+        var reader = new FileReader();
+        var textFile = /text.*/;
+        if (file.type.match(textFile)) reader.onload = function(event) {
+            preview.innerHTML = event.target.result;
+            highlightText("Em \xf4m ch\xfa v\xe0o l\xf2ng v\xe0 th\u1EE7 th\u1EC9:");
+        };
+        else preview.innerHTML = "<span class='error'>It doesn't seem to be a text file!</span>";
+        reader.readAsText(file);
+    }
+    function highlightText(textSearch) {
+        var content = document.getElementById("show-text").innerHTML;
+        var searchRegEx = new RegExp(textSearch, "gi");
+        var matches = content.match(searchRegEx);
+        console.log("content", content);
+        if (matches && matches.length > 0) {
+            // Làm nổi bật tất cả các khớp
+            var replaceText = `<span class="highlight">${textSearch}</span>`;
+            var highlightedContent = content.replace(searchRegEx, replaceText);
+            document.getElementById("show-text").innerHTML = highlightedContent;
+            // Cuộn đến vị trí của khớp đầu tiên
+            var firstOccurrence = document.querySelector(".highlight");
+            if (firstOccurrence) firstOccurrence.scrollIntoView();
+        } else console.log("No matches found");
+    }
+} else alert("Your browser is too old to support HTML5 File API");
+
+//# sourceMappingURL=index.579125c3.js.map
---
+++ src/index.js
@@ -0,0 +1,48 @@
+// Check for the various File API support.
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  function showFile() {
+     var preview = document.getElementById('show-text');
+     var file = document.querySelector('input[type=file]').files[0];
+     var reader = new FileReader()
+
+     var textFile = /text.*/;
+
+     if (file.type.match(textFile)) {
+        reader.onload = function (event) {
+           preview.innerHTML = event.target.result;
+           highlightText("Em ôm chú vào lòng và thủ thỉ:")
+
+        }
+
+     } else {
+        preview.innerHTML = "<span class='error'>It doesn't seem to be a text file!</span>";
+     }
+     reader.readAsText(file);
+  }
+
+  function highlightText(textSearch) {
+    var content = document.getElementById('show-text').innerHTML;
+    var searchRegEx = new RegExp(textSearch, "gi");
+    var matches = content.match(searchRegEx);
+    console.log('content', content)
+    
+    if (matches && matches.length > 0) {
+      // Làm nổi bật tất cả các khớp
+      var replaceText = `<span class="highlight">${textSearch}</span>`;
+      var highlightedContent = content.replace(searchRegEx, replaceText);
+      document.getElementById('show-text').innerHTML = highlightedContent;
+      
+      // Cuộn đến vị trí của khớp đầu tiên
+      var firstOccurrence = document.querySelector('.highlight');
+      if (firstOccurrence) {
+        firstOccurrence.scrollIntoView();
+      }
+    } else {
+      console.log("No matches found");
+    }
+  }
+
+} else {
+  alert("Your browser is too old to support HTML5 File API");
+}
+
